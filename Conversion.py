a7ca8709963c56a6f408bc146fdbfd61fce250f5
Ernafandeu <johannerna0@gmail.com>
2022-08-10 21:12:22 +0200

    Mon nouveau projet

---
+++ Conversion.py
@@ -0,0 +1,41 @@
+print("Bienvenue dans cet espace destiné à la conversion des chiffres en lettres")
+print("Quelle valeur voulez-vous traduire?")
+
+A=int(input())
+
+if A == 0:
+    print("La conversion de ce chiffre en lettre est : Zéro")
+
+if A == 1:
+    print("La conversion de ce chiffre en lettre est : Un")
+
+if A == 2:
+    print("La conversion de ce chiffre en lettre est : Deux")
+
+if A == 3:
+     print("La conversion de ce chiffre en lettre est : Trois")
+
+if A == 4:
+    print("La conversion de ce chiffre en lettre est : Quatre")
+
+if A == 5:
+    print("La conversion de ce chiffre en lettre est : Cinq")
+
+if A == 6:
+    print("La conversion de ce chiffre en lettre est : Six")
+
+if A == 7:
+    print("La conversion de ce chiffre en lettre est : Sept")
+
+if A == 8:
+    print("La conversion de ce chiffre en lettre est : Huit")
+
+if A == 9:
+    print("La conversion de ce chiffre en lettre est : Neuf")
+
+
+#def dix() :    dix = dixaine/ 1 chiffre en unité
+
+#def cent() :   cent = centaine/ 2 chiffres en unité
+
+#def mill() :    mill = millième/ 3 chiffres en unité
